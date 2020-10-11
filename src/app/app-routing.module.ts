5e2280f1002d8cffb7406166195c4c9bc99cc18f
NicolasSoler <josolerh@poligran.edu.co>
2020-10-11 03:27:50 -0500

    Arreglo en el ruter y bootones enlazados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { CreateTweetComponent } from './create-tweet/create-tweet.component';
 import { TweetsListComponent } from './tweets-list/tweets-list.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/listaTweets', pathMatch: 'full'},
+  {path: '', redirectTo: 'listaTweets', pathMatch: 'full'},
   {path: 'listaTweets', component: TweetsListComponent},
   {path: 'crearTweet', component: CreateTweetComponent}
 ];
